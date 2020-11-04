96a240931284275b8f238a5347cdee6c0c82d092
DNAngeluS <luciomf@gmail.com>
2020-11-04 20:51:48 -0300

    Update docker-entrypoint.sh
    Force renew link from app dir

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,8 +4,7 @@ set -euo pipefail
 cp -r /usr/src/moodle /app
 chown -R root:www-data /app /data
 chmod -R 775 /app/moodle /data
-rmdir /var/www/html
-ln -s /app/moodle /var/www/html
+ln -fs /app/moodle /var/www/html
 echo "* * * * *    /usr/bin/php /var/www/html/admin/cli/cron.php > /dev/null" > /app/cron
 crontab /app/cron
 
